dd82060c42d884a6dee649e5095d0d345ce07ac4
EmersonAguilar <eneftaly1911@gmail.com>
2023-08-13 13:54:28 -0600

    Agregar archivos de proyecto.

---
+++ ScanerURl/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ScanerURl;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ScanerURl/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ScanerURl;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
